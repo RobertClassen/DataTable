09527c16f93ce199991a70ec7aef57f699fec871
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2021-09-15 23:46:16 +0200

    added Clear method

--- Runtime/Scripts/DataTable.cs
+++ Runtime/Scripts/DataTable.cs
@@ -56,6 +56,22 @@ namespace DataTypes
 		#endregion
 
 		#region Methods
+		/// <summary>
+		/// Resets the values of all <see cref ="cells"/> to their <c>default(T)</c> value.
+		/// </summary>
+		public void Clear()
+		{
+			int xMax = XMax;
+			int yMax = YMax;
+			for(int y = YMin; y <= yMax; y++)
+			{
+				for(int x = XMin; x <= xMax; x++)
+				{
+					cells[x][y] = default(T);
+				}
+			}
+		}
+
 		public void Resize(int width, int height)
 		{
 			if(width <= Int.Zero)

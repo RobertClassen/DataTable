8c5329885d9afe0c09dcac2cc0f582cc8a393503
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2021-09-15 23:39:50 +0200

    added Flip method

--- Runtime/Scripts/DataTable.Transform.Rotate.cs
+++ Runtime/Scripts/DataTable.Transform.Rotate.cs
@@ -22,6 +22,45 @@ namespace DataTypes
 
 		#region Methods
 		/// <summary>
+		/// Switches the X and Y axes of the <see cref ="cells"/>, 
+		/// swapping their <see cref ="Width"/> and <see cref ="Height"/>.
+		/// </summary>
+		/// 
+		/// <remarks>
+		/// Before:<br/>
+		/// <c>
+		/// . ▼ ▼ ▼ ▼ ▼<br/>
+		/// ► 0 1 2 3 4<br/>
+		/// ► 5 6 7 8 9<br/>
+		/// ► A B C D E<br/>
+		/// </c>
+		/// 
+		/// After:<br/>
+		/// <c>
+		/// . ▼ ▼ ▼<br/>
+		/// ► 0 5 A<br/>
+		/// ► 1 6 B<br/>
+		/// ► 2 7 C<br/>
+		/// ► 3 8 D<br/>
+		/// ► 4 9 E<br/>
+		/// </c>
+		/// </remarks>
+		public void Flip()
+		{
+			int xMax = XMax;
+			int yMax = YMax;
+			DataTable<T> flipped = new DataTable<T>(Height, Width);
+			for(int y = YMin; y <= yMax; y++)
+			{
+				for(int x = XMin; x <= xMax; x++)
+				{
+					flipped.cells[x][y] = cells[y][x];
+				}
+			}
+			cells = flipped.cells;
+		}
+
+		/// <summary>
 		/// Rotates the <see cref ="cells"/> counterclockwise, 
 		/// swapping their <see cref ="Width"/> and <see cref ="Height"/>.
 		/// </summary>

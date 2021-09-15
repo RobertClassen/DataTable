f06e0e7d934f3eaf38757f86c61a955b4b54c155
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2021-09-15 23:19:36 +0200

    moved verification methods to core class

--- Runtime/Scripts/DataTable.Core.cs
+++ Runtime/Scripts/DataTable.Core.cs
@@ -9,6 +9,7 @@ namespace DataTypes.Core
 	public abstract class DataTable
 	{
 		#region Constants
+		private const string ArgumentOutOfRange = "Specified argument was out of the range of valid values.";
 		/// <summary>
 		/// The minimum valid column index.
 		/// </summary>
@@ -49,19 +50,76 @@ namespace DataTypes.Core
 		#endregion
 
 		#region Methods
+		/// <summary>
+		/// Checks if the specified <c>x</c> is a valid column index.
+		/// </summary>
 		public bool IsClampedX(int x)
 		{
-			return x.IsClamped(Int.Zero, Width - Int.One);
+			return x.IsClamped(XMin, XMax);
 		}
 
+		/// <summary>
+		/// Checks if the specified <c>y</c> is a valid row index.
+		/// </summary>
 		public bool IsClampedY(int y)
 		{
-			return y.IsClamped(Int.Zero, Height - Int.One);
+			return y.IsClamped(YMin, YMax);
 		}
 
+		/// <summary>
+		/// Checks if the specified <c>x</c> and <c>y</c> are valid column and row indices.
+		/// </summary>
 		public bool IsClamped(int x, int y)
 		{
 			return IsClampedX(x) && IsClampedY(y);
+		}
+
+		protected void VerifyXMinMax(int xMin, int xMax)
+		{
+			if(!IsClampedX(xMin))
+			{
+				throw new ArgumentOutOfRangeException(nameof(xMin), xMin, ArgumentOutOfRange);
+			}
+			if(!IsClampedX(xMax))
+			{
+				throw new ArgumentOutOfRangeException(nameof(xMax), xMax, ArgumentOutOfRange);
+			}
+			if(xMin > xMax)
+			{
+				throw new ArgumentException(string.Format("{0} must be less than or equal to {1}", nameof(xMin), nameof(xMax)));
+			}
+		}
+
+		protected void VerifyYMinMax(int yMin, int yMax)
+		{
+			if(!IsClampedY(yMin))
+			{
+				throw new ArgumentOutOfRangeException(nameof(yMin), yMin, ArgumentOutOfRange);
+			}
+			if(!IsClampedY(yMax))
+			{
+				throw new ArgumentOutOfRangeException(nameof(yMax), yMax, ArgumentOutOfRange);
+			}
+			if(yMin > yMax)
+			{
+				throw new ArgumentException(string.Format("{0} must be less than or equal to {1}", nameof(yMin), nameof(yMax)));
+			}
+		}
+
+		protected void VerifyX(int x)
+		{
+			if(!IsClampedX(x))
+			{
+				throw new ArgumentOutOfRangeException(nameof(x), x, ArgumentOutOfRange);
+			}
+		}
+
+		protected void VerifyY(int y)
+		{
+			if(!IsClampedY(y))
+			{
+				throw new ArgumentOutOfRangeException(nameof(y), y, ArgumentOutOfRange);
+			}
 		}
 		#endregion
 	}

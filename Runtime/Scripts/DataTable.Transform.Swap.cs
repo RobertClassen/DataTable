8d996dbb2a423f5faf294d86030c36876e3ca288
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2021-09-15 23:32:40 +0200

    added methods to swap cells, columns, and rows

---
+++ Runtime/Scripts/DataTable.Transform.Swap.cs
@@ -0,0 +1,217 @@
+namespace DataTypes
+{
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.Extensions;
+	using System.Utilities;
+
+	public partial class DataTable<T>
+	{
+		#region Fields
+		
+		#endregion
+
+		#region Properties
+		
+		#endregion
+
+		#region Constructors
+		
+		#endregion
+
+		#region Methods
+		/// <summary>
+		/// Swaps the values of two individual cells.
+		/// </summary>
+		/// 
+		/// <remarks>
+		/// Use <see cref ="SwapColumns"/> or <see cref ="SwapRows"/> 
+		/// if entire columns or rows need to be swapped at once.
+		/// 
+		/// <example>
+		/// <code>
+		/// //DataTable<char> chars = ...
+		/// chars.Swap(0, 0, 4, 2);
+		/// </code>
+		/// </example>
+		/// 
+		/// Before:<br/>
+		/// <c>
+		/// . ▼ . . . . .<br/>
+		/// ► 0 1 2 3 4 .<br/>
+		/// . 5 6 7 8 9 .<br/>
+		/// . A B C D E ◄<br/>
+		/// . . . . . ▲ .<br/>
+		/// </c>
+		/// 
+		/// After:<br/>
+		/// <c>
+		/// . ▼ . . . . .<br/>
+		/// ► E 1 2 3 4 .<br/>
+		/// . 5 6 7 8 9 .<br/>
+		/// . A B C D 0 ◄<br/>
+		/// . . . . . ▲ .<br/>
+		/// </c>
+		/// </remarks>
+		public void Swap(int x0, int y0, int x1, int y1)
+		{
+			T temp = cells[x0][y0];
+			cells[x0][y0] = cells[x1][y1];
+			cells[x1][y1] = temp;
+		}
+
+		/// <summary>
+		/// Swaps the values of the entire columns <c>x0</c> and <c>x1</c>.
+		/// </summary>
+		/// 
+		/// <remarks>
+		/// <example>
+		/// <code>
+		/// //DataTable<char> chars = ...
+		/// chars.SwapColumns(0, 1);
+		/// </code>
+		/// </example>
+		/// 
+		/// Before:<br/>
+		/// <c>
+		/// ▼ ▼ . . .<br/>
+		/// 0 1 2 3 4<br/>
+		/// 5 6 7 8 9<br/>
+		/// A B C D E<br/>
+		/// </c>
+		/// 
+		/// After:<br/>
+		/// <c>
+		/// ▼ ▼ . . .<br/>
+		/// 1 0 2 3 4<br/>
+		/// 6 5 7 8 9<br/>
+		/// B A C D E<br/>
+		/// </c>
+		/// </remarks>
+		public void SwapColumns(int x0, int x1)
+		{
+			SwapColumns(x0, x1, YMin, YMax);
+		}
+
+		/// <summary>
+		/// Swaps the values of the columns <c>x0</c> and <c>x1</c> 
+		/// between the rows <c>yMin</c> and <c>yMax</c>, inclusively.
+		/// </summary>
+		/// 
+		/// <remarks>
+		/// <example>
+		/// <code>
+		/// //DataTable<char> chars = ...
+		/// chars.SwapColumns(0, 1, 0, 1);
+		/// </code>
+		/// </example>
+		/// 
+		/// Before:<br/>
+		/// <c>
+		/// . ▼ ▼ . . .<br/>
+		/// ► 0 1 2 3 4<br/>
+		/// ► 5 6 7 8 9<br/>
+		/// . A B C D E<br/>
+		/// </c>
+		/// 
+		/// After:<br/>
+		/// <c>
+		/// . ▼ ▼ . . .<br/>
+		/// ► 1 0 2 3 4<br/>
+		/// ► 6 5 7 8 9<br/>
+		/// . A B C D E<br/>
+		/// </c>
+		/// </remarks>
+		public void SwapColumns(int x0, int x1, int yMin, int yMax)
+		{
+			VerifyX(x0);
+			VerifyX(x1);
+			VerifyYMinMax(yMin, yMax);
+
+			T temp;
+			for(int y = yMin; y <= yMax; y++)
+			{
+				temp = cells[x0][y];
+				cells[x0][y] = cells[x1][y];
+				cells[x1][y] = temp;
+			}
+		}
+
+		/// <summary>
+		/// Swaps the values of the entire rows <c>y0</c> and <c>y1</c>.
+		/// </summary>
+		/// 
+		/// <remarks>
+		/// <example>
+		/// <code>
+		/// //DataTable<char> chars = ...
+		/// chars.SwapRows(0, 1);
+		/// </code>
+		/// </example>
+		/// 
+		/// Before:<br/>
+		/// <c>
+		/// ► 0 1 2 3 4<br/>
+		/// ► 5 6 7 8 9<br/>
+		/// . A B C D E<br/>
+		/// </c>
+		/// 
+		/// After:<br/>
+		/// <c>
+		/// ► 5 6 7 8 9<br/>
+		/// ► 0 1 2 3 4<br/>
+		/// . A B C D E<br/>
+		/// </c>
+		/// </remarks>
+		public void SwapRows(int y0, int y1)
+		{
+			SwapRows(y0, y1, XMin, XMax);
+		}
+
+		/// <summary>
+		/// Swaps the values of the rows <c>y0</c> and <c>y1</c> 
+		/// between the columns <c>xMin</c> and <c>xMax</c>, inclusively.
+		/// </summary>
+		/// 
+		/// <remarks>
+		/// <example>
+		/// <code>
+		/// //DataTable<char> chars = ...
+		/// chars.SwapRows(0, 1, 0, 1);
+		/// </code>
+		/// </example>
+		/// 
+		/// Before:<br/>
+		/// <c>
+		/// . ▼ ▼ . . .<br/>
+		/// ► 0 1 2 3 4<br/>
+		/// ► 5 6 7 8 9<br/>
+		/// . A B C D E<br/>
+		/// </c>
+		/// 
+		/// After:<br/>
+		/// <c>
+		/// . ▼ ▼ . . .<br/>
+		/// ► 5 6 2 3 4<br/>
+		/// ► 0 1 7 8 9<br/>
+		/// . A B C D E<br/>
+		/// </c>
+		/// </remarks>
+		public void SwapRows(int y0, int y1, int xMin, int xMax)
+		{
+			VerifyY(y0);
+			VerifyY(y1);
+			VerifyXMinMax(xMin, xMax);
+
+			T temp;
+			for(int x = xMin; x <= xMax; x++)
+			{
+				temp = cells[x][y0];
+				cells[x][y0] = cells[x][y1];
+				cells[x][y1] = temp;
+			}
+		}
+		#endregion
+	}
+}

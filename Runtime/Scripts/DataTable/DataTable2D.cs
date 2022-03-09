73cbdfbbce5b4c1cfabd842883a71c5b5ffc3e0a
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2022-03-10 00:08:06 +0100

    added DataTable2D class

---
+++ Runtime/Scripts/DataTable/DataTable2D.cs
@@ -0,0 +1,51 @@
+namespace DataTypes
+{
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	public class DataTable2D<T> : DataTable<T>
+	{
+		#region Fields
+
+		#endregion
+
+		#region Properties
+		public T this[Vector2Int coordinate]
+		{
+			get { return rows[coordinate.y][coordinate.x]; }
+			set { rows[coordinate.y][coordinate.x] = value; }
+		}
+
+		public Vector2Int Size
+		{ get { return new Vector2Int(Width, Height); } }
+
+		public RectInt Rect
+		{ get { return ToRectInt(Size - Vector2Int.one); } }
+		#endregion
+
+		#region Constructors
+		public DataTable2D(Vector2Int size) : base(size.x, size.y)
+		{
+		}
+		#endregion
+
+		#region Methods
+		public bool IsClamped(Vector2Int coordinate)
+		{
+			return IsClampedX(coordinate.x) && IsClampedY(coordinate.y);
+		}
+
+		public void Resize(Vector2Int size)
+		{
+			Resize(size.x, size.y);
+		}
+
+		private static RectInt ToRectInt(Vector2Int size, Vector2Int position = default(Vector2Int))
+		{
+			return new RectInt(position, size);
+		}
+		#endregion
+	}
+}

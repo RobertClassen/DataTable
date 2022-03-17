276100e3d9daad37f8ba60c86a8d503578656f4a
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2022-03-17 23:52:19 +0100

    fixed possible bug due to incorrect check for same size; added comments

--- Runtime/Scripts/DataTable/DataTable.Resize.cs
+++ Runtime/Scripts/DataTable/DataTable.Resize.cs
@@ -7,18 +7,32 @@ namespace DataTypes
 
 	public partial class DataTable<T>
 	{
+		/// <summary>
+		/// Changes the number of columns and rows to the specified <c>width</c> and <c>height</c> values 
+		/// if a resize is required.
+		/// </summary>
+		/// 
+		/// <remarks>
+		/// If they are smaller than the old sizes the extra <see cref ="cells"/> are removed.<br/>
+		/// 
+		/// If they are larger than the old sizes new <see cref ="cells"/> are added 
+		/// and set to the <c>default(T)</c> value.<br/>
+		/// </remarks>
+		/// 
+		/// <param name="width">The new number of columns.</param>
+		/// <param name="height">The new number of rows.</param>
 		public void Resize(int width, int height)
 		{
 			if(width <= Int.Zero)
 			{
 				throw new ArgumentLessEqualsZeroException(nameof(width), width);
 			}
-			else if(height <= Int.Zero)
+			if(height <= Int.Zero)
 			{
 				throw new ArgumentLessEqualsZeroException(nameof(height), height);
 			}
 
-			if(width == Width || height == Height)
+			if(width == Width && height == Height)
 			{
 				return;
 			}

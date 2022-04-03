f110af5b0840ba91055dc74405628eef2f0b799e
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2022-04-03 12:47:16 +0200

    added parameterless method overloads for AddColumn and AddRow

--- Runtime/Scripts/DataTable/DataTable.Add.cs
+++ Runtime/Scripts/DataTable/DataTable.Add.cs
@@ -8,27 +8,14 @@ namespace DataTypes
 	public partial class DataTable<T>
 	{
 		/// <summary>
-		/// Adds the <c>items</c> in a new column on the right.
+		/// Adds a new empty column on the right.
 		/// </summary>
-		/// <remarks>
-		/// Items which are out of bounds will be ignored.
-		/// </remarks>
-		/// <param name="items">The items to be added.</param>
-		/// <param name="yOffset">Offsets the items by the specified number of rows.</param>
-		/// <returns>Returns the same instance.</returns>
-		public DataTable<T> AddColumn(IList<T> items, int yOffset = Int.Zero)
+		public DataTable<T> AddColumn()
 		{
-			for(int y = YMin; y <= YMax; y++)
+			int yMax = YMax;
+			for(int y = YMin; y <= yMax; y++)
 			{
 				rows[y].Cells.Add(default(T));
-			}
-			int yMin = yOffset.Max(YMin);
-			int yMax = (Height + yOffset).Min(YMax);
-			int iMin = (-yOffset).Max(YMin);
-			int iMax = (items.Count - yOffset).Min(items.Count - Int.One);
-			for(int y = yMin, i = iMin; y <= yMax && i <= iMax; y++, i++)
-			{
-				rows[y].Cells[XMax] = items[i];
 			}
 			return this;
 		}
@@ -37,7 +24,35 @@ namespace DataTypes
 		/// Adds the <c>items</c> in a new column on the right.
 		/// </summary>
 		/// <remarks>
-		/// Items which are out of bounds will be ignored.
+		/// Items which are out of bounds will be ignored.  
+		/// The new column will be added first to avoid the internal jagged <c>List</c> becoming misaligned 
+		/// in case of an exception when assigning the <c>items</c>.
+		/// </remarks>
+		/// <param name="items">The items to be added.</param>
+		/// <param name="yOffset">Offsets the items by the specified number of rows.</param>
+		/// <returns>Returns the same instance.</returns>
+		public DataTable<T> AddColumn(IList<T> items, int yOffset = Int.Zero)
+		{
+			AddColumn();
+			int yMin = yOffset.Max(YMin);
+			int yMax = (Height + yOffset).Min(YMax);
+			int iMin = (-yOffset).Max(YMin);
+			int iMax = (items.Count - yOffset).Min(items.Count - Int.One);
+			int xMax = XMax;
+			for(int y = yMin, i = iMin; y <= yMax && i <= iMax; y++, i++)
+			{
+				rows[y][xMax] = items[i];
+			}
+			return this;
+		}
+
+		/// <summary>
+		/// Adds the <c>items</c> in a new column on the right.
+		/// </summary>
+		/// <remarks>
+		/// Items which are out of bounds will be ignored.  
+		/// The new column will be added first to avoid the internal jagged <c>List</c> becoming misaligned 
+		/// in case of an exception when assigning the <c>items</c>.
 		/// </remarks>
 		/// <param name="items">The items to be added.</param>
 		/// <returns>Returns the same instance.</returns>
@@ -47,17 +62,28 @@ namespace DataTypes
 		}
 
 		/// <summary>
+		/// Adds a new empty row at the bottom.
+		/// </summary>
+		public DataTable<T> AddRow()
+		{
+			rows.Add(new Row(Width));
+			return this;
+		}
+
+		/// <summary>
 		/// Adds the <c>items</c> in a new row at the bottom.
 		/// </summary>
 		/// <remarks>
-		/// Items which are out of bounds will be ignored.
+		/// Items which are out of bounds will be ignored.  
+		/// The new row will be added first to match the behavior of the <see cref ="AddColumn"/> method.
 		/// </remarks>
 		/// <param name="items">The items to be added.</param>
 		/// <param name="xOffset">Offsets the items by the specified number of columns.</param>
 		/// <returns>Returns the same instance.</returns>
 		public DataTable<T> AddRow(IList<T> items, int xOffset = Int.Zero)
 		{
-			Row row = new Row(Width);
+			AddRow();
+			Row row = rows[YMax];
 			int xMin = xOffset.Max(XMin);
 			int xMax = (Width + xOffset).Min(XMax);
 			int iMin = (-xOffset).Max(XMin);
@@ -66,7 +92,6 @@ namespace DataTypes
 			{
 				row[x] = items[i];
 			}
-			rows.Add(row);
 			return this;
 		}
 
@@ -74,7 +99,8 @@ namespace DataTypes
 		/// Adds the <c>items</c> in a new row at the bottom.
 		/// </summary>
 		/// <remarks>
-		/// Items which are out of bounds will be ignored.
+		/// Items which are out of bounds will be ignored.  
+		/// The new row will be added first to match the behavior of the <see cref ="AddColumn"/> method.
 		/// </remarks>
 		/// <param name="items">The items to be added.</param>
 		/// <returns>Returns the same instance.</returns>

313649db4a005a98d53d6779c1ba7f0c57493edd
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2022-04-13 13:06:17 +0200

    added more Clear method overloads

--- Runtime/Scripts/DataTable/DataTable.Clear.cs
+++ Runtime/Scripts/DataTable/DataTable.Clear.cs
@@ -7,18 +7,103 @@ namespace DataTypes
 	public partial class DataTable<T>
 	{
 		/// <summary>
-		/// Resets the values of all <see cref ="cells"/> to their <c>default(T)</c> value.
+		/// Resets the values of all <c>cells</c> to their <c>default(T)</c> value.
 		/// </summary>
 		/// <returns>The same instance.</returns>
 		public DataTable<T> Clear()
 		{
-			int xMax = XMax;
-			int yMax = YMax;
-			for(int y = YMin; y <= yMax; y++)
+			return ClearRows(YMin, YMax, XMin, XMax);
+		}
+
+		/// <summary>
+		/// Resets the values of the <c>cells</c> in column <c>x</c> to their <c>default(T)</c> value 
+		/// (for rows <c>yMin</c> to <see cref ="YMax"/>).
+		/// </summary>
+		/// <returns>The same instance.</returns>
+		public DataTable<T> ClearColumn(int x, int yMin = YMin)
+		{
+			return ClearColumn(x, yMin, YMax);
+		}
+
+		/// <summary>
+		/// Resets the values of the <c>cells</c> in column <c>x</c> to their <c>default(T)</c> value 
+		/// (for rows <c>yMin</c> to <c>yMax</c>).
+		/// </summary>
+		/// <returns>The same instance.</returns>
+		public DataTable<T> ClearColumn(int x, int yMin, int yMax)
+		{
+			return ClearColumns(x, x, yMin, yMax);
+		}
+
+		/// <summary>
+		/// Resets the values of the <c>cells</c> in columns <c>xMin</c> to <c>xMax</c> 
+		/// to their <c>default(T)</c> value 
+		/// (for rows <c>yMin</c> to <see cref ="YMax"/>).
+		/// </summary>
+		/// <returns>The same instance.</returns>
+		public DataTable<T> ClearColumns(int xMin, int xMax, int yMin = YMin)
+		{
+			return ClearColumns(xMin, xMax, yMin, YMax);
+		}
+
+		/// <summary>
+		/// Resets the values of the <c>cells</c> in columns <c>xMin</c> to <c>xMax</c> 
+		/// to their <c>default(T)</c> value 
+		/// (for rows <c>yMin</c> to <c>yMax</c>).
+		/// </summary>
+		/// <returns>The same instance.</returns>
+		public DataTable<T> ClearColumns(int xMin, int xMax, int yMin, int yMax)
+		{
+			return ClearRows(yMin, yMax, xMin, xMax);
+		}
+
+		/// <summary>
+		/// Resets the values of the <c>cells</c> in row <c>y</c> to their <c>default(T)</c> value 
+		/// (for rows <c>xMin</c> to <see cref ="XMax"/>).
+		/// </summary>
+		/// <returns>The same instance.</returns>
+		public DataTable<T> ClearRow(int y, int yMin = YMin)
+		{
+			return ClearRow(y, yMin, YMax);
+		}
+
+		/// <summary>
+		/// Resets the values of the <c>cells</c> in row <c>y</c> to their <c>default(T)</c> value 
+		/// (for rows <c>xMin</c> to <c>xMax</c>).
+		/// </summary>
+		/// <returns>The same instance.</returns>
+		public DataTable<T> ClearRow(int y, int xMin, int xMax)
+		{
+			return ClearRows(y, y, xMin, xMax);
+		}
+
+		/// <summary>
+		/// Resets the values of the <c>cells</c> in rows <c>yMin</c> to <c>yMax</c> 
+		/// to their <c>default(T)</c> value 
+		/// (for rows <c>xMin</c> to <see cref ="XMax"/>).
+		/// </summary>
+		/// <returns>The same instance.</returns>
+		public DataTable<T> ClearRows(int yMin, int yMax, int xMin = XMin)
+		{
+			return ClearRows(yMin, yMax, xMin, XMax);
+		}
+
+		/// <summary>
+		/// Resets the values of the <c>cells</c> in rows <c>yMin</c> to <c>yMax</c> 
+		/// to their <c>default(T)</c> value 
+		/// (for rows <c>xMin</c> to <c>xMax</c>).
+		/// </summary>
+		/// <returns>The same instance.</returns>
+		public DataTable<T> ClearRows(int yMin, int yMax, int xMin, int xMax)
+		{
+			VerifyXMinMax(xMin, xMax);
+			VerifyYMinMax(yMin, yMax);
+			for(int y = yMin; y <= yMax; y++)
 			{
-				for(int x = XMin; x <= xMax; x++)
+				Row row = rows[y];
+				for(int x = xMin; x <= xMax; x++)
 				{
-					rows[y][x] = default(T);
+					row[x] = default(T);
 				}
 			}
 			return this;

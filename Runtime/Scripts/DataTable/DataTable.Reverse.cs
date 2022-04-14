ac16449187c152ea12618201c11b43b55a625557
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2022-04-14 15:18:07 +0200

    added Reverse method overloads with parameters for partial columns and rows

--- Runtime/Scripts/DataTable/DataTable.Reverse.cs
+++ Runtime/Scripts/DataTable/DataTable.Reverse.cs
@@ -37,16 +37,19 @@ namespace DataTypes
 			int halfCapacity = Capacity / Two;
 			int count = Int.Zero;
 
+			Row current;
+			Row opposite;
 			T temp;
 			for(int y = YMin; y <= yMax; y++)
 			{
-				int yReverse = yMax - y;
+				current = rows[y];
+				opposite = rows[yMax - y];
 				for(int x = XMin; x <= xMax; x++)
 				{
 					int xReverse = xMax - x;
-					temp = rows[y][x];
-					rows[y][x] = rows[yReverse][xReverse];
-					rows[yReverse][xReverse] = temp;
+					temp = current[x];
+					current[x] = opposite[xReverse];
+					opposite[xReverse] = temp;
 
 					if(++count >= halfCapacity)
 					{
@@ -81,24 +84,11 @@ namespace DataTypes
 		/// <returns>The same instance.</returns>
 		public DataTable<T> ReverseColumn(int x)
 		{
-			VerifyX(x);
-
-			int yMax = YMax;
-			int yHalf = yMax / Two;
-
-			T temp;
-			for(int y = YMin; y <= yHalf; y++)
-			{
-				int yReverse = yMax - y;
-				temp = rows[y][x];
-				rows[y][x] = rows[yReverse][x];
-				rows[yReverse][x] = temp;
-			}
-			return this;
+			return ReverseColumns(x, x);
 		}
 
 		/// <summary>
-		/// Switches 'up' and 'down' of all columns.
+		/// Switches 'up' and 'down' of all columns from <c>xMin</c> to the last one.
 		/// </summary>
 		/// 
 		/// <remarks>
@@ -119,21 +109,51 @@ namespace DataTypes
 		/// </c>
 		/// </remarks>
 		/// <returns>The same instance.</returns>
-		public DataTable<T> ReverseColumns()
+		public DataTable<T> ReverseColumns(int xMin = XMin)
 		{
-			int xMax = XMax;
+			return ReverseColumns(xMin, XMax);
+		}
+
+		/// <summary>
+		/// Switches 'up' and 'down' of all columns from <c>xMin</c> to <c>xMax</c>, inclusively.
+		/// </summary>
+		/// 
+		/// <remarks>
+		/// Before:<br/>
+		/// <c>
+		/// ▼ ▼ ▼ ▼ ▼<br/>
+		/// 0 1 2 3 4<br/>
+		/// 5 6 7 8 9<br/>
+		/// A B C D E<br/>
+		/// </c>
+		/// 
+		/// After:<br/>
+		/// <c>
+		/// A B C D E<br/>
+		/// 5 6 7 8 9<br/>
+		/// 0 1 2 3 4<br/>
+		/// ▲ ▲ ▲ ▲ ▲<br/>
+		/// </c>
+		/// </remarks>
+		/// <returns>The same instance.</returns>
+		public DataTable<T> ReverseColumns(int xMin, int xMax)
+		{
+			VerifyXMinMax(xMin, xMax);
 			int yMax = YMax;
 			int yHalf = yMax / Two;
 
+			Row current;
+			Row opposite;
 			T temp;
 			for(int y = YMin; y <= yHalf; y++)
 			{
-				int yReverse = yMax - y;
-				for(int x = XMin; x <= xMax; x++)
+				current = rows[y];
+				opposite = rows[yMax - y];
+				for(int x = xMin; x <= xMax; x++)
 				{
-					temp = rows[y][x];
-					rows[y][x] = rows[yReverse][x];
-					rows[yReverse][x] = temp;
+					temp = current[x];
+					current[x] = opposite[x];
+					opposite[x] = temp;
 				}
 			}
 			return this;
@@ -161,24 +181,11 @@ namespace DataTypes
 		/// <returns>The same instance.</returns>
 		public DataTable<T> ReverseRow(int y)
 		{
-			VerifyY(y);
-
-			int xMax = XMax;
-			int xHalf = xMax / Two;
-
-			T temp;
-			for(int x = XMin; x <= xHalf; x++)
-			{
-				int xReverse = xMax - x;
-				temp = rows[y][x];
-				rows[y][x] = rows[y][xReverse];
-				rows[y][xReverse] = temp;
-			}
-			return this;
+			return ReverseRows(y, y);
 		}
 
 		/// <summary>
-		/// Switches 'left' and 'right' of all rows.
+		/// Switches 'left' and 'right' of all rows from <c>yMin</c> to the last one.
 		/// </summary>
 		/// 
 		/// <remarks>
@@ -197,21 +204,49 @@ namespace DataTypes
 		/// </c>
 		/// </remarks>
 		/// <returns>The same instance.</returns>
-		public DataTable<T> ReverseRows()
+		public DataTable<T> ReverseRows(int yMin = YMin)
 		{
+			return ReverseRows(yMin, YMax);
+		}
+
+		/// <summary>
+		/// Switches 'left' and 'right' of all rows from <c>yMin</c> to <c>yMax</c>, inclusively.
+		/// </summary>
+		/// 
+		/// <remarks>
+		/// Before:<br/>
+		/// <c>
+		/// ► 0 1 2 3 4<br/>
+		/// ► 5 6 7 8 9<br/>
+		/// ► A B C D E<br/>
+		/// </c>
+		/// 
+		/// After:<br/>
+		/// <c>
+		/// 4 3 2 1 0 ◄<br/>
+		/// 9 8 7 6 5 ◄<br/>
+		/// E D C B A ◄<br/>
+		/// </c>
+		/// </remarks>
+		/// <returns>The same instance.</returns>
+		public DataTable<T> ReverseRows(int yMin, int yMax)
+		{
+			VerifyYMinMax(yMin, yMax);
+
 			int xMax = XMax;
-			int yMax = YMax;
 			int xHalf = xMax / Two;
 
+			Row row;
 			T temp;
-			for(int x = XMin; x <= xHalf; x++)
+			for(int y = yMin; y <= yMax; y++)
 			{
-				int xReverse = xMax - x;
-				for(int y = YMin; y <= yMax; y++)
+				row = rows[y];
+				for(int x = XMin; x <= xHalf; x++)
 				{
-					temp = rows[y][x];
-					rows[y][x] = rows[y][xReverse];
-					rows[y][xReverse] = temp;
+					int xReverse = xMax - x;
+					temp = row[x];
+					row[x] = row[xReverse];
+					row[xReverse] = temp;
 				}
 			}
 			return this;

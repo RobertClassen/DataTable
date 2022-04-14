0c7a6e25d66474a0db406135b3505f54f7515163
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2022-04-14 20:36:17 +0200

    updated tests

--- Tests/Editor/Scripts/DataTableTests.GetHashCode.cs
+++ Tests/Editor/Scripts/DataTableTests.GetHashCode.cs
@@ -18,9 +18,15 @@ namespace DataTypes
 			Assert.AreEqual(-243236825, table.GetColumnHashCode(2));
 			Assert.AreEqual(-327334870, table.GetColumnHashCode(3));
 			Assert.AreEqual(-748338927, table.GetColumnHashCode(4));
+		}
+
+		[Test]
+		public void GetColumnHashCode_Null_DoesNotThrow()
+		{
+			DataTable<string> table = Create5x3();
 
 			table[0, 0] = null;
-			Assert.AreEqual(-385921419, table.GetColumnHashCode(0));
+			Assert.DoesNotThrow(() => table.GetColumnHashCode(0));
 		}
 
 		[Test]
@@ -31,9 +37,15 @@ namespace DataTypes
 			Assert.AreEqual(311891762, table.GetRowHashCode(0));
 			Assert.AreEqual(-1665475355, table.GetRowHashCode(1));
 			Assert.AreEqual(-1848596599, table.GetRowHashCode(2));
+		}
+
+		[Test]
+		public void GetRowHashCode_Null_DoesNotThrow()
+		{
+			DataTable<string> table = Create5x3();
 
 			table[0, 0] = null;
-			Assert.AreEqual(745453362, table.GetRowHashCode(0));
+			Assert.DoesNotThrow(() => table.GetRowHashCode(0));
 		}
 	}
 }
